ff2b34a73b208e4336ffb261e1977aca9bcfb918
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-09-02 02:29:52 +0600

    done

--- src/test.js
+++ src/test.js
@@ -1,15 +1,1 @@
-const response = await fetch("https://api.deepseek.com/chat/completions", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${API_KEY}`
-  },
-  body: JSON.stringify({
-    model: "deepseek-v4-flash",
-    messages: 
-      {
-        role: "user",
-        content: code
-      }
-  })
-});
+const x = 0;
